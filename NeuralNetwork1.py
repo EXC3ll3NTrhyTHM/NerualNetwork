5157c72b0c1d5c8563c7c63dfb770abfcde3fa1e
Blake Simpson <shadowcloud131@gmail.com>
2021-05-01 11:12:53 -0500

    Gave values for missing entries, passed dataset into linear estimator and got 75% accuracy

--- NeuralNetwork1.py
+++ NeuralNetwork1.py
@@ -9,6 +9,14 @@ import matplotlib.pyplot as plt
 dftrain = pd.read_csv('aug_train.csv')
 # head() shows first 5 entries
 print(dftrain.head())
+dftrain['gender'].fillna(value='unknown', inplace=True)
+dftrain['enrolled_university'].fillna(value='unknown', inplace=True)
+dftrain['education_level'].fillna(value='unknown', inplace=True)
+dftrain['major_discipline'].fillna(value='unknown', inplace=True)
+dftrain['experience'].fillna(value='unknown', inplace=True)
+dftrain['company_size'].fillna(value='unknown', inplace=True)
+dftrain['company_type'].fillna(value='unknown', inplace=True)
+dftrain['last_new_job'].fillna(value='unknown', inplace=True)
 
 # Storing the output that will be evaluated against later
 output_train  = dftrain.pop('target')
@@ -26,8 +34,9 @@ print(output_train)
 #plt.show()
 # seems to show that more females are looking for a job than males
 
+# Column with non-numerical values. will need to associate numerical values to them
 CATEGORICAL_COLUMNS = ['city', 'gender', 'relevent_experience', 'enrolled_university', 'education_level', 'major_discipline', 'experience', 'company_size', 'company_type', 'last_new_job']
-NUMERIC_COLUMNS = ['enrollee_id','city_development', 'training_hours']
+NUMERIC_COLUMNS = ['enrollee_id','city_development_index', 'training_hours']
 
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
@@ -38,3 +47,25 @@ for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 print(feature_columns)
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, output_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+# This eval input is the exact same as the training input which can lead to the neural network to just memorize the data set and if I were to give it a slightly different dataset to evaluate it might perform a lot worse
+eval_input_fn = make_input_fn(dftrain, output_train, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+# We create a linear estimtor by passing the feature columns we created earlier
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+#clear_output()  # clears console output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
