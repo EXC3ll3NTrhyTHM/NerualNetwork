bb7acb556d093231d689cdf49ea74e2389585dc0
Blake Simpson <shadowcloud131@gmail.com>
2021-05-08 20:14:01 -0500

    Data converted to only numerical values

--- NeuralNetwork1.py
+++ NeuralNetwork1.py
@@ -1,79 +1,61 @@
-# A lot of the code is from this tutorial https://colab.research.google.com/drive/15Cyy2H7nT40sGR7TBN5wBvgTd57mVKay#forceEdit=true&sandboxMode=true&scrollTo=-lcnwG0VXF5h
+import os
+# Reduces python logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 # Loads source dataset into data frame
 # data frames allows specific columns to be referenced
-dftrain = pd.read_csv('aug_train.csv')
-# head() shows first 5 entries
-#print(dftrain.head())
-dftrain['gender'].fillna(value='unknown', inplace=True)
-dftrain['enrolled_university'].fillna(value='unknown', inplace=True)
-dftrain['education_level'].fillna(value='unknown', inplace=True)
-dftrain['major_discipline'].fillna(value='unknown', inplace=True)
-dftrain['experience'].fillna(value='unknown', inplace=True)
-dftrain['company_size'].fillna(value='unknown', inplace=True)
-dftrain['company_type'].fillna(value='unknown', inplace=True)
-dftrain['last_new_job'].fillna(value='unknown', inplace=True)
+df = pd.read_csv('aug_train.csv')
+# Shuffle the data
+# sample essentially randomizes data from dataset into new order using frac=1 which treats the whole dataset as the sameple size
+df.sample(frac=1)
+
+# Fill in empty values as the string unknown
+df['gender'].fillna(value='unknown', inplace=True)
+df['enrolled_university'].fillna(value='unknown', inplace=True)
+df['education_level'].fillna(value='unknown', inplace=True)
+df['major_discipline'].fillna(value='unknown', inplace=True)
+df['experience'].fillna(value='unknown', inplace=True)
+df['company_size'].fillna(value='unknown', inplace=True)
+df['company_type'].fillna(value='unknown', inplace=True)
+df['last_new_job'].fillna(value='unknown', inplace=True)
+
+# A lot of this code came from here https://pbpython.com/categorical-encoding.html
+df = df.replace({'relevent_experience': {'Has relevent experience':1,'No relevent experience':0}})
+df = df.replace({'experience': {'>20':21,'<1':0, 'unknown':0}})
+df['experience'] = pd.to_numeric(df['experience'])
+df = df.replace({'last_new_job': {'>4':5,'never':0, 'unknown':0}})
+df['last_new_job'] = pd.to_numeric(df['last_new_job'])
+df['city'] = df['city'].str.replace('city_', '').astype(int)
+
+#print(df['city'].value_counts())
+
+#print(df.dtypes)
+
+gender_cat = pd.get_dummies(df['gender'], prefix='gender')
+univer_cat = pd.get_dummies(df['enrolled_university'], prefix='univer')
+edu_cat = pd.get_dummies(df['education_level'], prefix='edu')
+major_cat = pd.get_dummies(df['major_discipline'], prefix='major')
+comp_size_cat = pd.get_dummies(df['company_size'], prefix='comp_size')
+comp_type_cat = pd.get_dummies(df['company_type'], prefix='comp_type')
+
+#print(comp_type_cat.head())
+
+df.drop(['gender','enrolled_university','education_level','major_discipline','company_size','company_type'], axis=1,inplace=True)
+df = pd.concat((df, gender_cat, univer_cat, edu_cat, major_cat, comp_size_cat, comp_type_cat), axis=1)
+
+train, test = train_test_split(df, test_size=0.2)
+#print(test.head())
 
 # Storing the output that will be evaluated against later
-output_train  = dftrain.pop('target')
-# show dataset without target column
-#print(dftrain.head())
-# show target column, target value is whether or not they are indeed looking for a job
-#print(output_train)
+output_train  = train.pop('target')
+output_test = test.pop('target')
 
-# Create histogram showing the distribution of training hours across the dataset
-#dftrain.gender.hist(bins=10)
-#plt.show()
-
-# Creates hostogram showing the males, females and others that need a job
-#pd.concat([dftrain, output_train], axis=1).groupby('gender').target.mean().plot(kind='barh').set_xlabel('% need job')
-#plt.show()
-# seems to show that more females are looking for a job than males
-
-# Column with non-numerical values. will need to associate numerical values to them
-CATEGORICAL_COLUMNS = ['city', 'gender', 'relevent_experience', 'enrolled_university', 'education_level', 'major_discipline', 'experience', 'company_size', 'company_type', 'last_new_job']
-NUMERIC_COLUMNS = ['enrollee_id','city_development_index', 'training_hours']
-
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-    vocabulary = dftrain[feature_name].unique() # gets list of unique entries for each field
-    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
-
-for feature_name in NUMERIC_COLUMNS:
-  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
-
-#print(feature_columns)
-
-def make_input_fn(data_df, label_df, num_epochs=20, shuffle=True, batch_size=32):
-  def input_function():  # inner function, this will be returned
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
-    if shuffle:
-      ds = ds.shuffle(1000)  # randomize order of data
-    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
-    return ds  # return a batch of the dataset
-  return input_function  # return a function object for use
-
-train_input_fn = make_input_fn(dftrain, output_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
-# This eval input is the exact same as the training input which can lead to the neural network to just memorize the data set and if I were to give it a slightly different dataset to evaluate it might perform a lot worse
-eval_input_fn = make_input_fn(dftrain, output_train, num_epochs=1, shuffle=False)
-
-print("Something1")
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
-# We create a linear estimtor by passing the feature columns we created earlier
-print("Something2")
-linear_est.train(train_input_fn)  # train
-print("Something3")
-result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
-
-print(result['accuracy'])  # the result variable is simply a dict of stats about our model
-print(result)
-
-print("Something4")
-result = list(linear_est.predict(eval_input_fn))
-print(dftrain.loc[0])
-print(output_train[0])
-print(result[0]['probabilities'][0])
+print(train.shape)
